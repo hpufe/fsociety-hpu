f66509106a78fb713a68a56faec5c1815c1a3be4
jeneser <jeneserwang@gmail.com>
2017-09-08 18:24:10 +0800

    Add shell script: training-it

---
+++ training/training-it/training-it.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Run Tesseract for Training
+tesseract hpu.font.exp0.tif hpu.font.exp0 box.train
+
+# Generate the unicharset file
+unicharset_extractor hpu.font.exp0.box
+
+# Mftraining
+mftraining -F font_properties -U unicharset -O hpu.unicharset hpu.font.exp0.tr
+
+# Cntraining...
+cntraining hpu.font.exp0.tr
+
+# Rename
+mv normproto hpu.normproto
+mv inttemp hpu.inttemp
+mv pffmtable hpu.pffmtable
+mv shapetable hpu.shapetable
+
+# Create Tessdata
+combine_tessdata hpu.
+
+# ok!
