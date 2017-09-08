4f3cf4ed560da3f91a6a6a207f35d119bb287552
jeneser <jeneserwang@gmail.com>
2017-09-08 15:16:16 +0800

    Fix citt

--- tools/convert-img-to-tif.js
+++ tools/convert-img-to-tif.js
@@ -2,22 +2,22 @@ var denodeify = require('denodeify');
 var path = require('path');
 var fs = require('fs');
 var gm = require('gm');
-var ora = require('ora' );
+var ora = require('ora');
 
 var readdir = denodeify(fs.readdir);
 
 // Config
 var config = {
-  src: '../training/samples/',
-  dist: '../training/samples-output/',
+  src: path.join(__dirname, '../training/samples/'),
+  dist: path.join(__dirname, '../training/samples-output/'),
   // Reduces the image contrast
   contrast: -100
-}
+};
 
 var spinner = ora('Start conversion...').start();
 
 // Convert it
-readdir(path.join(__dirname, config.src))
+readdir(config.src)
   .catch(err => {
     return Promise.reject(err);
   })
@@ -28,28 +28,23 @@ readdir(path.join(__dirname, config.src))
       // Reduces the speckles within the image.
       // Reduces the image contrast
       // Convert images to .tif
-      gm(path.join(__dirname, config.src, files[i]))
+      gm(config.src + files[i])
         .despeckle()
         .contrast(config.contrast)
         .resize(240, 80)
-        .write(
-          path.join(
-            __dirname,
-            config.dist,
-            files[i].split('.')[0] + '.tif'
-          ),
-          err => {
-            if (err) {
-              console.log(err);
-            }
+        .write(config.dist + files[i].split('.')[0] + '.tif', err => {
+          if (err) {
+            console.log(err);
           }
-        );
+        });
 
-      if(i === l - 1) {
-        spinner.succeed('Successful conversion! Let the bullets fly for a while...')
+      if (i === l - 1) {
+        spinner.succeed(
+          'Successful conversion! Let the bullets fly for a while...'
+        );
       }
     }
   })
   .catch(err => {
     console.log(err);
-  })
+  });
